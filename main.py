74ab3fa96930782db10d282b739ffcf00c7b0bf1
Kyungtae Bae <Kyungtae@marvrus.com>
2022-04-11 19:13:34 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def string_operator():
+    print("이름\t나이\t지역")
+    print("쿠카\t55\t서울")
+    print("쭝이\t22\t경기")
+    print("이든\t4\t제주")
+
+
+def variable():
+    pi = 3.14159265
+    r = 10
+
+    print("원주율 =", pi)
+    print("반지름 =", r)
+    print("원의 둘레 =", 2 * pi * r)
+    print("원의 넓이 =", pi * r * r)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    variable()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
